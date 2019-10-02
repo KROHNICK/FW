5e0c87c7b12cd9641165fac4af36120745bd921b
KROHNICK <business.kanekperez@gmail.com>
2019-10-02 11:35:18 -0700

    set shopOrigin in cookes from user's session, installed app bridge react package and js cookes to read cookies in pages/_app,js, added provider component

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import App from "next/app";
 import Head from "next/head";
 import { AppProvider } from "@shopify/polaris";
+import { Provider } from "@shopify/app-bridge-react";
 import "@shopify/polaris/styles.css";
 
 class MyApp extends App {
@@ -12,9 +13,11 @@ class MyApp extends App {
           <title>Sample App</title>
           <meta charSet="utf-8" />
         </Head>
-        <AppProvider>
-          <Component {...pageProps} />
-        </AppProvider>
+        <Provider>
+          <AppProvider>
+            <Component {...pageProps} />
+          </AppProvider>
+        </Provider>
       </React.Fragment>
     );
   }
