2d82167c9d21705ee639b3d9dba93af5b2718251
KROHNICK <business.kanekperez@gmail.com>
2019-10-02 10:09:48 -0700

    added hack to next.config to exclude 'minimize' from dev server on yarn dev command, tested polaris import css with text styles in pages

--- next.config.js
+++ next.config.js
@@ -11,3 +11,25 @@ module.exports = withCSS({
     return config;
   }
 });
+
+function HACK_removeMinimizeOptionFromCssLoaders(config) {
+  console.warn(
+    "HACK: Removing `minimize` option from `css-loader` entries in Webpack config"
+  );
+  config.module.rules.forEach(rule => {
+    if (Array.isArray(rule.use)) {
+      rule.use.forEach(u => {
+        if (u.loader === "css-loader" && u.options) {
+          delete u.options.minimize;
+        }
+      });
+    }
+  });
+}
+
+module.exports = withCSS({
+  webpack(config) {
+    HACK_removeMinimizeOptionFromCssLoaders(config);
+    return config;
+  }
+});
